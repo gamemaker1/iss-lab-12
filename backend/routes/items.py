b27a63e7993114ae2cadc3b9e68395f3808321ce
Devashree <devashree1236@gmail.com>
2025-04-16 16:06:06 +0530

    resolved few issues in items.py

--- backend/routes/items.py
+++ backend/routes/items.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from models import Item
 from bson import ObjectId
+from db import init_db
 
-router = APIRouter()
+router = APIRouter(prefix="/items")
 
 async def get_items_collection():
     from db import init_db
@@ -24,11 +25,11 @@ async def create_item(item: Item):
     return {"id": str(result.inserted_id)}
 
 # I want a chocolate
-@router.delete("/{item_id}/{item_details}")
-async def delete_item(item_id: str, item_details:str):
+@router.delete("/{item_id}")
+async def delete_item(item_id: str):
     collection = await get_items_collection()
     result = await collection.delete_one({"_id": ObjectId(item_id)})
-    result2 = await collection.delete_one({"_id": ObjectId(item_details)})
+    
     if result.deleted_count:
-        return {"status": "deleted", "deleted_item":result2}
+        return {"status": "deleted"}
     raise HTTPException(status_code=404, detail="Item not found")
